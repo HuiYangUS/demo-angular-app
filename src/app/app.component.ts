739886d91ac7ec64594681dca74f73b6709e321c
Hui Yang <hui.yang.9045@gmail.com>
2025-07-04 15:23:55 -0400

    added directive practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DataBinderComponent } from './data-binder/data-binder.component';
+import { DirectiveDemoComponent } from './directive-demo/directive-demo.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    UserComponent,
-    DataBinderComponent
+    // UserComponent,
+    // DataBinderComponent,
+    DirectiveDemoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
---
+++ src/app/directive-demo/directive-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-demo',
+  imports: [],
+  templateUrl: './directive-demo.component.html',
+  styleUrl: './directive-demo.component.scss'
+})
+export class DirectiveDemoComponent {
+
+}
