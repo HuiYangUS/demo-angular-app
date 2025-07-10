ed88d443e6e3c78ccfe8c598bc06f4ba021c85c5
Hui Yang <hui.yang.9045@gmail.com>
2025-07-10 17:01:27 -0400

    created home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { DataBinderComponent } from './data-binder/data-binder.component';
 import { StructuralDirectiveDemoComponent } from './structural-directive-demo/structural-directive-demo.component';
 import { AttributeDirectiveDemoComponent } from './attribute-directive-demo/attribute-directive-demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyHomeComponent } from './my-home/my-home.component';
 
 /**
  * <Route> is an object
  */
 export const routes: Routes = [
     // { path: "", component: UserComponent },
-    { path: "", redirectTo: "user", pathMatch: "full" },
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "home", component: MyHomeComponent },
     { path: "user", component: UserComponent },
     { path: "data-binding", component: DataBinderComponent },
     { path: "structural-directive", component: StructuralDirectiveDemoComponent },
