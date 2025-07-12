85b755cb2d3b725ba34fb066a6b9e41cabe1a4bf
Hui Yang <hui.yang.9045@gmail.com>
2025-07-12 15:56:35 -0400

    added demo hooks

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
@@ -13,13 +13,71 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   username: string = "";
   password: string = "";
 
   constructor(private router: Router) {
+    console.log("<0>: LoginComponent has been created.")
+  }
 
+  /**
+   * Use: for input properties
+   * @param changes 
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    // throw new Error('Method not implemented.');
+    console.log("<1>: ngOnChanges has been called.")
+  }
+
+  /**
+   * Use (!): for API calls
+   */
+  ngOnInit(): void {
+    console.log("<2>: ngOnInit() has been called.")
+  }
+
+  /**
+   * Use: for change detection (every time a change is made)
+   */
+  ngDoCheck(): void {
+    console.log("<3>: ngDoCheck() has been called.")
+  }
+
+  /**
+   * Use: for creation of ng-Content
+   */
+  ngAfterContentInit(): void {
+    console.log("<4>: ngAfterContentInit() has been called.")
+  }
+
+  /**
+   * Use: for checking on ng-Content
+   */
+  ngAfterContentChecked(): void {
+    console.log("<5>: ngAfterContentChecked() has been called.")
+  }
+
+  /**
+   * Use (!): for viewChild (content has been rendered)
+   */
+  ngAfterViewInit(): void {
+    console.log("<6>: ngAfterViewInit() has been called.")
+  }
+
+  /**
+   * Use (!): for checking on viewChild
+   */
+  ngAfterViewChecked(): void {
+    console.log("<7>: ngAfterViewChecked() has been called.")
+  }
+
+  /**
+   * Use (!): for when this component is destroyed
+   */
+  ngOnDestroy(): void {
+    console.log("<8>: ngOnDestroy() has been called.")
   }
 
   loginMyApp() {
