89d19e1b3c67de94f18eebb004239a856efbe67b
Hui Yang <hui.yang.9045@gmail.com>
2025-07-13 09:16:47 -0400

    completed built in pipe demo

--- src/app/built-in-pipes/built-in-pipes.component.ts
+++ src/app/built-in-pipes/built-in-pipes.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-built-in-pipes',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './built-in-pipes.component.html',
   styleUrl: './built-in-pipes.component.scss'
 })
@@ -11,5 +13,22 @@ export class BuiltInPipesComponent {
 
   // Use: 'component.html'
   ngPipeDef: string = "pipes are functions that are used to format data in angular templates";
+
+  personData = {
+    id: 101,
+    name: "John Smith",
+    score: 97,
+    class: "Biology"
+  };
+
+  currentDate: Date = new Date();
+
+  pie: number = 3.14159;
+
+  items = of(["Apple", "Peach", "Mango"]);
+
+  demoOptions = ["String", "Number", "Raw Data"];
+
+  targetSelection = "";
 
 }
